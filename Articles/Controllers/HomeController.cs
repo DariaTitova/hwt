947084913359400a4cc8d6e3238afaa4fa7a7d35
Daria <dasha.tit@mail.ru>
2019-08-13 04:46:40 +0300

    add abstractions and interfaces

--- Articles/Controllers/HomeController.cs
+++ Articles/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Articles.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,10 @@ namespace Articles.Controllers
         public ActionResult Index()
         {
             var session = NHibernateHelper.OpenSession();
+            session.Save(new Cataloges()
+            {
+                Name = "root1"
+            });
             return View();
         }
 
---
+++ Articles/Scripts/AbstractMenyItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Articles.Scripts
+{
+    public abstract class AbstractMenyItem
+    {
+        private string name;
+        public AbstractMenyItem(string Name)
+        {
+            this.Name = Name;
+        }
+        public string Name { get => name; set => name = value; }
+    }
+}
