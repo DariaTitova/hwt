38b8761cc5d2fd4455950faf1a3bec0b3eb03cad
Daria <dasha.tit@mail.ru>
2019-08-13 07:58:47 +0300

    add ioc container

--- Articles/Controllers/HomeController.cs
+++ Articles/Controllers/HomeController.cs
@@ -4,26 +4,38 @@ using Articles.Interfaces;
 using Articles.Items;
 using Articles.Models;
 using Articles.Scripts;
+using NHibernate;
+using Ninject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+ using System.Web;
 using System.Web.Mvc;
 
 namespace Articles.Controllers
 {
     public class HomeController : Controller
     {
+        IParentItem parent;
+        ISession session;
+        public HomeController()
+        {
+           session = NHibernateHelper.OpenSession();
+
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IParentItem>().To<CatalogesItems>()
+                .WithConstructorArgument("cataloge", session.Query<Cataloges>().ToList().First()); 
+            parent = ninjectKernel.Get<IParentItem>();
+        }
         public ActionResult Index()
         {
-            var session = NHibernateHelper.OpenSession();
+            session = NHibernateHelper.OpenSession();
             //session.Save(new Cataloges()
             //{
             //    Name = "root1"
             //});
 
-            Cataloges cataloge = session.Query<Cataloges>().ToList().First();
-            IParentItem parent = new CatalogesItems(cataloge);
+  
             HtmlGenerator generator = new HtmlGenerator(parent);
 
             ViewBag.Meny = new HtmlString(generator.GenerateMeny());
